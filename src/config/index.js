b5607a2a4217befe12727d5f167a6d08f9f89fdf
truecode112 <truecoder112@gmail.com>
2023-08-19 18:48:38 +0900

    Fixed deprecated issue

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export { default as AppTheme } from "./theme";
 export const API_PATH="https://goodmealtime.com/api.php"
-// export const API_PATH="http://192.168.123.56/test.php"
+//export const API_PATH="https://192.168.125.241/api.php"
 export const REFETCH=600000
 export const FAST_REFETCH=4000
 export const ITEM_HEIGHT=50 // TODO check
